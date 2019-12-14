7d9a1a66a648ac8e5238778b53f83cc12cde3f16
dmnc404 <fgaming386@gmail.com>
2019-12-14 14:14:32 +0700

    New Upload

---
+++ TelegramBot.js
@@ -0,0 +1,19 @@
+const Telegram = require('node-telegram-bot-api');
+const dbot     = require('dbot-js');
+const token    = process.env.TELEGRAM_TOKEN;
+
+const bot      = new Telegram(token, {
+    polling: true
+})
+
+
+bot.on('message', msg => {
+    const chatID  = msg.chat.id;
+    const message = msg.text.toString()
+
+    dbot.get_response(message, (err, response) => {
+        if(!err) {
+            bot.sendMessage(chatID, response)
+        }
+    })
+})
